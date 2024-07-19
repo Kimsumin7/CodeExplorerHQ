d27f33db36789cc064c718ee20ab44d49a5683a2
SooMin Kim <Kimsoominj@gmail.com>
2024-07-20 01:04:27 +0900

    쇼핑몰 메인페이지 완성

--- Complete Responsive Design/java.js
+++ Complete Responsive Design/java.js
@@ -3,3 +3,12 @@ const header = document.querySelector("header");
 window.addEventListener("scroll", function(){
     header.classList.toggle("sticky", this.window.scrollY > 0)
 })
+
+let menu = document.querySelector('#menu-icon');
+let navmenu = document.querySelector('.navmenu');
+
+//화면창 좁을 때만 오른쪽에 list나옴
+menu.onclick = () =>{
+    menu.classList.toggle('bx-x');
+    navmenu.classList.toggle('open');
+}
